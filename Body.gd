52ca137024d06c8a9ba7773781f0869a260d8be5
malceore <malceore@gmail.com>
2023-01-07 16:04:02 -0500

    made it so you can cut open the bodyand next it will spawn organs to harvest

--- Body.gd
+++ Body.gd
@@ -2,10 +2,12 @@ extends Area2D
 
 export var state = "initial"
 var initial_position = Vector2()
-var canvas
+var cutting = false
+var sprite
+var opened_graphic = preload("res://art/body_operation_open_sprite.png")
 
 func _ready():
-	canvas = get_node("CanvasLayer")
+	sprite = get_node("../Graphic")
 
 func _physics_process(_delta):
 	update()
@@ -13,10 +15,32 @@ func _physics_process(_delta):
 func _on_Body_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton:
 		# On click
-		if event.button_index == BUTTON_LEFT and event.pressed:
-			initial_position = get_local_mouse_position()
+		if event.button_index == BUTTON_LEFT and event.pressed and cutting:
+			cutting = false
+			body_opened()
+			print("1")
+			
 		# On release
-		elif event.button_index == BUTTON_LEFT and !event.pressed:
-			draw_line(initial_position, get_local_mouse_position(), Color(255, 0, 0), 10)
-			print(initial_position, get_local_mouse_position())
-			#canvas.draw(initial_position, position)
+		elif event.button_index == BUTTON_LEFT and !event.pressed :
+			initial_position = get_local_mouse_position()
+			print("2")
+			_draw()
+			cutting = true
+			
+			
+func body_opened():
+	state = "open"	
+	sprite.set_texture(opened_graphic)
+	#spawn_organs()
+	
+	
+func spawn_organs():
+	var organs = load("res://organs.tscn")
+	var organ_instance = organs.instance()
+	get_parent().add_child(organ_instance)
+			
+			
+func _draw():
+	if cutting and state == "initial":
+		draw_line(initial_position, get_local_mouse_position(), Color(1, 0, 0), 5)
+	

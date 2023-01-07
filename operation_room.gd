e519b9180b4febc09938d037419d9d938e461e49
malceore <malceore@gmail.com>
2023-01-06 20:20:07 -0500

    added dragable organs.

--- operation_room.gd
+++ operation_room.gd
@@ -1,4 +1,10 @@
 extends Node2D
 
+func _ready():
+	spawnOrgans()
+
+func spawnOrgans():
+	print("spawned em")
+
 func _on_Button3_pressed():
 	get_tree().change_scene_to(load('res://basement.tscn'))

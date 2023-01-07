2e4326d0c0ecc4c5eb8ed600db113dea5f011cd6
malceore <malceore@gmail.com>
2023-01-07 14:58:23 -0500

    removed bugging wav file and added a progress/delay to goon fetching

--- GoonButton.gd
+++ GoonButton.gd
@@ -1,6 +1,25 @@
 extends Button
 
-func _on_GoonButton_pressed():
+export var debug = false
+var timer = Timer.new() 
+var ProgressBar
+
+func _ready():
+	ProgressBar = get_node("../ProgressBar")
+
+func _on_GoonButton_pressed(): 
+	timer.connect("timeout",self,"fetch_body") 
+	timer.wait_time = 2
+	timer.one_shot = true
+	add_child(timer)
+	timer.start()
+
+
+func _process(delta):
+	ProgressBar.value = 10 * (timer.time_left / (timer.wait_time - timer.time_left))
+
+func fetch_body():
+	print("On my way to the graveyard!")
 	var bodies = get_tree().get_nodes_in_group("bodies")
 	for body in bodies:
 		if !body.occupied:
--- operation_room.gd
+++ operation_room.gd
@@ -5,12 +5,9 @@ var current_body
 func _ready():
 	spawnOrgans()
 
-
 func spawnOrgans():
 	print("spawned em")
-
 
 func _on_Button3_pressed():
 	current_body.toggle_occupied()
 	self.queue_free()
-

d3f9db9a76540a7aa1ac3ac08daf77e7e8a835af
malceore <malceore@gmail.com>
2023-01-06 19:53:21 -0500

    added operating room wiht plaaceholder sprite, can goon some bodies and take them to operation room for disection

--- body.gd
+++ body.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 var active = true
+var harvested = false
 
 func _ready():
 	toggleActive()
@@ -9,9 +10,12 @@ func _ready():
 func toggleActive():
 	if active:
 		$graphic.visible = false
+		$open_button.visible = false
 		active = false
 	else:
 		$graphic.visible = true
+		$open_button.visible = true
 		active = true		
 
-
+func _on_open_button_pressed():
+	get_tree().change_scene_to(load('res://operation_room.tscn'))
